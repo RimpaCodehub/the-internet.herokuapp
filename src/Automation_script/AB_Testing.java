ce6cc619bd4c6c75515927b971d8d6ee27db479d
RimpaIT <rimpa.itweb@gmail.com>
2026-05-16 14:02:31 +0530

    Added

---
+++ src/Automation_script/AB_Testing.java
@@ -0,0 +1,25 @@
+package Automation_script;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class AB_Testing {
+public static void main(String[] args)  {
+	System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
+	//Launch the Browser
+	WebDriver driver = new FirefoxDriver();
+	
+	//Maximize the Window
+	driver.manage().window().maximize();
+	
+	//Enter the URL
+	driver.get("https://the-internet.herokuapp.com/");
+	
+	//Click on A/B Testing Link
+	driver.findElement(By.xpath("//a[.='A/B Testing']")).click();
+	
+	//Close the Browser
+	driver.quit();
+}
+}
